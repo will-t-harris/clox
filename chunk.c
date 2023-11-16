4d73f012c312d6b930630f528bdcad96c8237ea0
Will Harris <will.t.harris@protonmail.com>
2023-11-16 10:54:00 -0800

    add constant instructions to bytecode

--- chunk.c
+++ chunk.c
@@ -3,16 +3,19 @@
 
 #include "chunk.h"
 #include "memory.h"
+#include "value.h"
 
 // create empty dynamic array
 void initChunk(Chunk *chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk *chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  freeValueArray(&chunk->constants);
 
   initChunk(chunk);
 }
@@ -31,4 +34,9 @@ void writeChunk(Chunk *chunk, uint8_t byte) {
 
   chunk->code[chunk->count] = byte;
   chunk->count++;
+}
+
+int addConstant(Chunk *chunk, Value value) {
+  writeValueArray(&chunk->constants, value);
+  return chunk->constants.count - 1;
 }
--- debug.c
+++ debug.c
@@ -5,6 +5,7 @@
 
 void disassembleChunk(Chunk *chunk, const char *name) {
   printf("== %s ==\n", name);
+
   for (int offset = 0; offset < chunk->count;) {
     // disassembleInstruction handles incrememting this loop's counter because
     // instructions can have different sizes
@@ -12,8 +13,19 @@ void disassembleChunk(Chunk *chunk, const char *name) {
   }
 }
 
+static int constantInstruction(const char *name, Chunk *chunk, int offset) {
+  uint8_t constant = chunk->code[offset + 1];
+
+  printf("%-16s %4d '", name, constant);
+  printValue(chunk->constants.values[constant]);
+  printf("'\n");
+
+  return offset + 2;
+}
+
 static int simpleInstruction(const char *name, int offset) {
   printf("%s\n", name);
+
   return offset + 1;
 }
 
@@ -22,6 +34,9 @@ int disassembleInstruction(Chunk *chunk, int offset) {
 
   uint8_t instruction = chunk->code[offset];
   switch (instruction) {
+  case OP_CONSTANT: {
+    return constantInstruction("OP_CONSTANT", chunk, offset);
+  }
   case OP_RETURN: {
     return simpleInstruction("OP_RETURN", offset);
   }
