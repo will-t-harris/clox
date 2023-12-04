9b11672a010bbb6e53adf7b266d3e44fd85443f9
Will Harris <will.t.harris@protonmail.com>
2023-12-04 10:38:31 -0800

    add simple bytecode generation to compiler backend

--- compiler.c
+++ compiler.c
@@ -13,6 +13,11 @@ typedef struct {
 } Parser;
 
 Parser parser;
+Chunk *compilingChunk;
+
+static Chunk *currentChunk() {
+  return compilingChunk;
+}
 
 static void errorAt(Token *token, const char *message) {
   if (parser.panicMode) {
@@ -64,8 +69,23 @@ static void consume(TokenType type, const char *message) {
   errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte) {
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+// emit opcode followed by a one-byte operand
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+  emitByte(byte1);
+  emitByte(byte2);
+}
+
+static void emitReturn() {
+  emitByte(OP_RETURN);
+}
+
 bool compile(const char *source, Chunk *chunk) {
   initScanner(source);
+  compilingChunk = chunk;
 
   parser.hadError = false;
   parser.panicMode = false;
@@ -73,5 +93,6 @@ bool compile(const char *source, Chunk *chunk) {
   advance();
   expression();
   consume(TOKEN_EOF, "Expect end of expression.");
+  endCompiler();
   return !parser.hadError;
 }
