9f0c8374ac89574c4503a63a2b8ce69aaf85216b
Will Harris <will.t.harris@protonmail.com>
2023-12-26 13:01:45 -0800

    use macros to implement dynamically-typed numbers

--- vm.c
+++ vm.c
@@ -4,11 +4,25 @@
 #include "compiler.h"
 #include "debug.h"
 #include "value.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 VM vm;
 
 static void resetStack() { vm.stackTop = vm.stack; }
+
+static void runtimeError(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  fputs("\n", stderr);
+
+  size_t instruction = vm.ip - vm.chunk->code - 1;
+  int line = vm.chunk->lines[instruction];
+  fprintf(stderr, "[line %d] in script\n", line);
+  resetStack();
+}
 
 void initVM() { resetStack(); }
 
@@ -24,18 +38,23 @@ Value pop() {
   return *vm.stackTop;
 }
 
+static Value peek(int distance) { return vm.stackTop[-1 - distance]; }
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 // this do/while pattern gives us a way to contain multiple statements inside
 // a block, while also permitting semicolons at the end
-#define BINARY_OP(op)                                                          \
+#define BINARY_OP(valueType, op)                                               \
   do {                                                                         \
-    double b = pop();                                                          \
-    double a = pop();                                                          \
-    push(a op b);                                                              \
-  } while (false)
-
+    if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) {                          \
+      runtimeError("Operands must be numbers.");                               \
+      return INTERPRET_RUNTIME_ERROR;                                          \
+    }                                                                          \
+    double b = AS_NUMBER(pop());                                               \
+    double a = AS_NUMBER(pop());                                               \
+    push(valueType(a op b));                                                   \
+  } while (false);
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
     printf("          ");
@@ -61,23 +80,28 @@ static InterpretResult run() {
       break;
     }
     case OP_ADD: {
-      BINARY_OP(+);
+      BINARY_OP(NUMBER_VAL, +);
       break;
     }
     case OP_SUBTRACT: {
-      BINARY_OP(-);
+      BINARY_OP(NUMBER_VAL, -);
       break;
     }
     case OP_MULTIPLY: {
-      BINARY_OP(*);
+      BINARY_OP(NUMBER_VAL, *);
       break;
     }
     case OP_DIVIDE: {
-      BINARY_OP(/);
+      BINARY_OP(NUMBER_VAL, /);
       break;
     }
     case OP_NEGATE: {
-      push(-pop());
+      if (!IS_NUMBER(peek(0))) {
+        runtimeError("Operand must be a number.");
+        return INTERPRET_RUNTIME_ERROR;
+      }
+
+      push(NUMBER_VAL(-AS_NUMBER(pop())));
       break;
     }
     case OP_RETURN: {
